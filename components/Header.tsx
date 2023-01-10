28bb16eb4c95d34546a04d47ee92da896029ae6e
David Almeida <contato.davidalmeida@outlook.com>
2023-01-10 20:50:51 -0300

    ✅ Update: Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,16 @@
-import Image from 'next/image';
 import { HiMenuAlt2 } from 'react-icons/hi';
 
 import Container from './Container';
-import styles from '../styles/Header.module.css';
+import Logo from './Logo';
 
-import Logo from '../public/images/marvel-avenger.png';
+import styles from '../styles/Header.module.css';
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <Container>
         <>
-          <div>
-            <figure>
-              <Image src={Logo} width="35" height="35" alt="Logo" />
-            </figure>
-            <h1>Marvel: Ultimate Guide</h1>
-          </div>
+          <Logo />
           <button type="button">
             <HiMenuAlt2 size={24} />
           </button>
