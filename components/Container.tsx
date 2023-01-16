04293a52aa2691f11e7eb62295524ed81eb50410
David Almeida <contato.davidalmeida@outlook.com>
2023-01-16 17:23:05 -0300

    ♻️ Refactor code

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,7 +1,7 @@
 import styles from '../styles/Container.module.css';
 
 interface ContainerProps {
-  children: React.ReactElement;
+  children?: React.ReactElement | React.ReactElement[];
 }
 
 export default function Container({ children }: ContainerProps) {
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,122 +12,120 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <Container>
-        <>
-          <div className={styles.group_nav}>
-            <h3>Início</h3>
-            <nav>
-              <ul>
-                <li>
-                  <Link href="/" target="_self" rel="noreferrer">
-                    Página Inicial
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/" target="_self" rel="noreferrer">
-                    Downloads
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/" target="_self" rel="noreferrer">
-                    Sobre
-                  </Link>
-                </li>
-              </ul>
-            </nav>
+        <div className={styles.group_nav}>
+          <h3>Início</h3>
+          <nav>
+            <ul>
+              <li>
+                <Link href="/" target="_self" rel="noreferrer">
+                  Página Inicial
+                </Link>
+              </li>
+              <li>
+                <Link href="/" target="_self" rel="noreferrer">
+                  Downloads
+                </Link>
+              </li>
+              <li>
+                <Link href="/" target="_self" rel="noreferrer">
+                  Sobre
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className={styles.group_nav}>
+          <h3>Sobre nós</h3>
+          <nav>
+            <ul>
+              <li>
+                <Link href="/" target="_self" rel="noreferrer">
+                  Informações gerais
+                </Link>
+              </li>
+              <li>
+                <Link href="/" target="_self" rel="noreferrer">
+                  Sobre o desenvolvimento
+                </Link>
+              </li>
+              <li>
+                <Link href="/" target="_self" rel="noreferrer">
+                  Junte-se a nós
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className={styles.group_nav}>
+          <h3>Suporte</h3>
+          <nav>
+            <ul>
+              <li>
+                <Link href="/" target="_self" rel="noreferrer">
+                  FAQ
+                </Link>
+              </li>
+              <li>
+                <Link href="/" target="_self" rel="noreferrer">
+                  Telefone
+                </Link>
+              </li>
+              <li>
+                <Link href="/" target="_self" rel="noreferrer">
+                  Chat
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className={styles.group_nav}>
+          <p>&copy; {currentYear} Copyright | David Almeida</p>
+          <nav>
+            <ul className={styles.links}>
+              <li>
+                <Link
+                  href="https://www.linkedin.com/in/davidalmeidadev/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <BsLinkedin size={24} />
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="https://discord.gg/RUx9WxZ"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <BsDiscord size={24} />
+                </Link>
+              </li>
+              <li>
+                <Link href="" target="_blank" rel="noreferrer">
+                  <BsWhatsapp size={24} />
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="https://www.instagram.com/davidalmeidadev/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <BsInstagram size={24} />
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <div className={styles.copyright}>
+            <Logo size={15} />
+            <span>
+              Todos os personagens da Marvel e suas semelhanças distintivas são
+              marcas registradas e direitos autorais © 1939–
+              {currentYear} Marvel Characters, Inc. Todos os direitos
+              reservados.
+            </span>
           </div>
-          <div className={styles.group_nav}>
-            <h3>Sobre nós</h3>
-            <nav>
-              <ul>
-                <li>
-                  <Link href="/" target="_self" rel="noreferrer">
-                    Informações gerais
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/" target="_self" rel="noreferrer">
-                    Sobre o desenvolvimento
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/" target="_self" rel="noreferrer">
-                    Junte-se a nós
-                  </Link>
-                </li>
-              </ul>
-            </nav>
-          </div>
-          <div className={styles.group_nav}>
-            <h3>Suporte</h3>
-            <nav>
-              <ul>
-                <li>
-                  <Link href="/" target="_self" rel="noreferrer">
-                    FAQ
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/" target="_self" rel="noreferrer">
-                    Telefone
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/" target="_self" rel="noreferrer">
-                    Chat
-                  </Link>
-                </li>
-              </ul>
-            </nav>
-          </div>
-          <div className={styles.group_nav}>
-            <p>&copy; {currentYear} Copyright | David Almeida</p>
-            <nav>
-              <ul className={styles.links}>
-                <li>
-                  <Link
-                    href="https://www.linkedin.com/in/davidalmeidadev/"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <BsLinkedin size={24} />
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="https://discord.gg/RUx9WxZ"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <BsDiscord size={24} />
-                  </Link>
-                </li>
-                <li>
-                  <Link href="" target="_blank" rel="noreferrer">
-                    <BsWhatsapp size={24} />
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="https://www.instagram.com/davidalmeidadev/"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <BsInstagram size={24} />
-                  </Link>
-                </li>
-              </ul>
-            </nav>
-            <div className={styles.copyright}>
-              <Logo size={15} />
-              <span>
-                Todos os personagens da Marvel e suas semelhanças distintivas
-                são marcas registradas e direitos autorais © 1939–
-                {currentYear} Marvel Characters, Inc. Todos os direitos
-                reservados.
-              </span>
-            </div>
-          </div>
-        </>
+        </div>
       </Container>
     </footer>
   );
