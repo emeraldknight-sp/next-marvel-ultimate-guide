a669c95416a3420e86c966add6e8f00da90427b7
David Almeida <contato.davidalmeida@outlook.com>
2023-01-16 17:20:17 -0300

    ✅ Update: Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,29 @@
-import { HiMenuAlt2 } from 'react-icons/hi';
+import { MdMenu } from 'react-icons/md';
 
 import Container from './Container';
 import Logo from './Logo';
 
 import styles from '../styles/Header.module.css';
+import { useState } from 'react';
+import SiderbarMenu from './Sidebar';
 
 export default function Header() {
+  const [open, setOpen] = useState(false);
+
+  const handleOpenMenu = (): void => {
+    console.log('Entrou como ', open);
+    setOpen(!open);
+    console.log('Saiu como ', !open);
+  };
+
   return (
     <header className={styles.header}>
+      <SiderbarMenu sidebarOpen={open} setSidebarOpen={handleOpenMenu} />
       <Container>
-        <>
-          <Logo />
-          <button type="button">
-            <HiMenuAlt2 size={24} />
-          </button>
-        </>
+        <Logo />
+        <button type="button" onClick={handleOpenMenu}>
+          <MdMenu size={24} />
+        </button>
       </Container>
     </header>
   );
