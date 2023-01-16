c649a443db223946f96af2b998c7f0c2cb8734af
David Almeida <contato.davidalmeida@outlook.com>
2023-01-16 17:20:36 -0300

    ✅ Update: Card component

--- components/Card.tsx
+++ components/Card.tsx
@@ -7,15 +7,29 @@ export default function Card({ char }: CharProps) {
   const [year, month, day] = char.modified.slice(0, 10).split('-');
   const avatar = `${char.thumbnail.path}.${char.thumbnail.extension}`;
   const date = `${day}/${month}/${year}`;
+  const nameLength = 11;
 
   return (
     <div className={styles.card_character}>
-      <div className={styles.card_title}>
-        <span>{char.id}</span>
-        <h3>{char.name}</h3>
-      </div>
+      {char.name.length > nameLength ? (
+        <div className={`${styles.card_title__xs}`}>
+          <span>{char.id}</span>
+          <h3>{char.name}</h3>
+        </div>
+      ) : (
+        <div className={styles.card_title}>
+          <span>{char.id}</span>
+          <h3>{char.name}</h3>
+        </div>
+      )}
       <figure className={styles.card_figure}>
-        <Image src={avatar} width="200" height="200" alt="avatar personagem" />
+        <Image
+          src={avatar}
+          width="200"
+          height="200"
+          alt="avatar personagem"
+          priority
+        />
       </figure>
       <div className={styles.card_details}>
         {/* <p>{char.description || 'Não possui descrição.'}</p> */}
