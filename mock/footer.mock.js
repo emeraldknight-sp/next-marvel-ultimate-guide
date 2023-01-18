eb7b0b3cdbca5b22b8b068644b7beae216e9f91a
David Almeida <contato.davidalmeida@outlook.com>
2023-01-18 18:55:17 -0300

    🤡 Mock things

---
+++ mock/footer.mock.js
@@ -0,0 +1,88 @@
+export const footer = {
+  getStarted: [
+    {
+      id: 1,
+      name: 'página inicial',
+      url: '/',
+      icon: <></>,
+    },
+    {
+      id: 2,
+      name: 'downloads',
+      url: '/',
+      icon: <></>,
+    },
+    {
+      id: 3,
+      name: 'sobre',
+      url: '/',
+      icon: <></>,
+    },
+  ],
+  about: [
+    {
+      id: 4,
+      name: 'informações gerais',
+      url: '/',
+      icon: <></>,
+    },
+    {
+      id: 5,
+      name: 'sobre o desenvolvimento',
+      url: '/',
+      icon: <></>,
+    },
+    {
+      id: 6,
+      name: 'junte-se a nós',
+      url: '/',
+      icon: <></>,
+    },
+  ],
+  support: [
+    {
+      id: 7,
+      name: 'FAQ',
+      url: '/',
+      icon: <></>,
+    },
+    {
+      id: 8,
+      name: 'telefone',
+      url: '/',
+      icon: <></>,
+    },
+    {
+      id: 9,
+      name: 'chat',
+      url: '/',
+      icon: <></>,
+    },
+  ],
+  contact: [
+    {
+      id: 10,
+      name: 'sobre',
+      url: 'https://www.linkedin.com/in/davidalmeidadev/',
+      icon: 'fa-brands fa-linkedin',
+    },
+    {
+      id: 11,
+      name: 'sobre',
+      url: 'https://discord.gg/RUx9WxZ',
+      icon: 'fa-brands fa-discord',
+    },
+    {
+      id: 12,
+      name: 'sobre',
+      url: '/',
+      icon: 'fa-brands fa-whatsapp',
+    },
+    {
+      id: 13,
+      name: 'sobre',
+      url: 'https://www.instagram.com/davidalmeidadev/',
+      icon: 'fa-brands fa-instagram',
+    },
+  ],
+};
---
+++ mock/sidebar.mock.js
@@ -0,0 +1,44 @@
+export const optionsMenuSidebar = [
+  {
+    id: 1,
+    name: 'dashboard',
+    url: '/',
+    icon: '',
+  },
+  {
+    id: 2,
+    name: 'shortcuts',
+    url: '/',
+    icon: '',
+  },
+  {
+    id: 3,
+    name: 'services',
+    url: '/',
+    icon: '',
+  },
+  {
+    id: 4,
+    name: 'overview',
+    url: '/',
+    icon: '',
+  },
+  {
+    id: 5,
+    name: 'contact',
+    url: '/',
+    icon: '',
+  },
+  {
+    id: 6,
+    name: 'events',
+    url: '/',
+    icon: '',
+  },
+  {
+    id: 7,
+    name: 'about',
+    url: '/',
+    icon: '',
+  },
+];
