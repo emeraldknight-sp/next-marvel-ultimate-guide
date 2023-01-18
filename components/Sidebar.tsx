27c4075bc0a997e8e7bff0311a8961fbcee6c26b
David Almeida <contato.davidalmeida@outlook.com>
2023-01-18 18:56:31 -0300

    ✅ Update: Sidebar component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,11 +1,13 @@
 import Link from 'next/link';
 import { MdClose } from 'react-icons/md';
 
+import { optionsMenuSidebar } from '../mock/sidebar.mock.js';
+
 import styles from '../styles/Sidebar.module.css';
 
 interface SidebarProps {
-  sidebarOpen: boolean;
-  setSidebarOpen: () => void;
+  sidebarOpen?: boolean;
+  setSidebarOpen?: () => void;
 }
 
 export default function SiderbarMenu({
@@ -13,7 +15,7 @@ export default function SiderbarMenu({
   setSidebarOpen,
 }: SidebarProps) {
   return (
-    <div className={styles.blocker}>
+    <>
       <div
         className={
           !sidebarOpen
@@ -22,56 +24,28 @@ export default function SiderbarMenu({
         }
       >
         <div>
-          <h3>MENU</h3>
+          <h3>Menu</h3>
           <button type="button" onClick={setSidebarOpen}>
             <MdClose size={24} />
           </button>
         </div>
         <nav>
           <ul>
-            <li>
-              <Link
-                href="https://www.google.com/"
-                target="_self"
-                rel="noreferrer"
-              >
-                Dashboard
-              </Link>
-            </li>
-            <li>
-              <Link href="/" target="_self" rel="noreferrer">
-                Shortcuts
-              </Link>
-            </li>
-            <li>
-              <Link href="/" target="_self" rel="noreferrer">
-                Services
-              </Link>
-            </li>
-            <li>
-              <Link href="/" target="_self" rel="noreferrer">
-                Overview
-              </Link>
-            </li>
-            <li>
-              <Link href="/" target="_self" rel="noreferrer">
-                Contact
-              </Link>
-            </li>
-            <li>
-              <Link href="/" target="_self" rel="noreferrer">
-                Events
-              </Link>
-            </li>
-            <li>
-              <Link href="/" target="_self" rel="noreferrer">
-                About
-              </Link>
-            </li>
+            {optionsMenuSidebar.map((item) => (
+              <li key={item.id}>
+                <Link
+                  href={item.url}
+                  target="_self"
+                  rel="noreferrer"
+                  onClick={() => console.log('Link')}
+                >
+                  {item.name}
+                </Link>
+              </li>
+            ))}
           </ul>
         </nav>
       </div>
-      {!sidebarOpen ? '' : <span></span>}
-    </div>
+    </>
   );
 }
